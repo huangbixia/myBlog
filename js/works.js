36579453a771aa2db7b48786ffb77e726cb5e8f0
huangbixia <15220007752@163.com>
2017-04-17 21:28:16 +0800

    2017-04-17 commited

---
+++ js/works.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function(){
+
+    //响应式菜单显示与隐藏
+    $("#switchBtn").toggle(function(){
+         $(".menuUl").addClass("showUl");
+    },function(){
+        $(".menuUl").removeClass("showUl");
+    });
+
+});
